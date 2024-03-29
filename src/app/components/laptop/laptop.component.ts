c9dd7a6c2f5ecba572f9588e46a79d8360dea488
Sydnee Snowball <s.snowball@hotmail.com>
2024-03-28 17:14:42 -0700

    some styling

---
+++ src/app/components/laptop/laptop.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-laptop',
+  standalone: true,
+  imports: [],
+  templateUrl: './laptop.component.html',
+  styleUrl: './laptop.component.scss'
+})
+export class LaptopComponent {
+  @Input() image: string | undefined;
+}
