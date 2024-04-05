9c3e5faa193d86939a3500776acb401db98e95fc
Sydnee Snowball <s.snowball@hotmail.com>
2024-04-05 10:22:02 -0700

    project details changes

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -10,12 +10,13 @@ export interface Project {
   'body': string;
   'url': string | null;
   'published_date': string | null;
-  'image': string | null;
+  'image_sm': string | null;
+  'image_lg': string | null;
   'thumb': string | null;
   'category_id': number | null;
   'created_at': string;
   'updated_at': string;
   'category': Category | null;
   'tags': Tag[];
-  'learning-outcomes': string[];
+  'learning_outcomes': string[];
 }
